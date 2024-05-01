0b42449a50f8e655e522ee552b2ffd49c7b9667e
amir <amir.student3232@gmail.com>
2024-05-01 20:28:59 +0300

    start project

--- yolo5/app.py
+++ yolo5/app.py
@@ -22,6 +22,44 @@ s3_client = boto3.client('s3', region_name=REGION_NAME)
 dynamo_client = boto3.client('dynamodb', region_name = REGION_NAME)
 
 
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "shermanawsproject"
+    region_name = "eu-north-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    return json.loads(secret)
+
+
+# load TELEGRAM_TOKEN value from Secret Manager
+secrets = get_secret()
+TELEGRAM_TOKEN = secrets["TELEGRAM_TOKEN"]  # os.environ['TELEGRAM_TOKEN']
+APP_URL = secrets["app_url"]  # os.environ['TELEGRAM_APP_URL']
+
 with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
 
